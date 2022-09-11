2dc5c4a30e4b3d5f6cebb3c561d1550cac279f05
Roman Fomin <rs.fomin@bpcbt.com>
2022-09-11 16:07:00 +0300

    add logic for employees items

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,25 +1,54 @@
+import { Component } from 'react';
 import './employees-list-item.css';
 
-const EmployeesListItem = ({name, salary, increase}) => {
+class EmployeesListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: false,
+      rise: false,
+    }
+  }
 
-  const increaseClass = increase === true ? 'increase' : ''
+  onIncrease = () => {
+    this.setState(({increase}) => ({  // аргумент {increase} = state
+      increase: !increase,
+    }));
+  };
 
-  return (
-    <li className={`list-group-item d-flex justify-content-between ${increaseClass}`}>
-      <span className="list-group-item-label">{name}</span>
-      <input type="text" className="list-group-item-input" defaultValue={`${salary}$`} />
-      <div className="d-flex justify-content-center align-items-center">
-        <button type="button" className="btn-cookie btn-sm ">
-          <i className="fas fa-cookie"></i>
-        </button>
+  onClick = () => {
+    this.setState(({rise}) => ({
+      rise: !rise,
+    }));
+  };
 
-        <button type="button" className="btn-trash btn-sm ">
-          <i className="fas fa-trash"></i>
-        </button>
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  );
+  render() {
+    const {name, salary} = this.props;
+    const {increase, rise} = this.state;
+
+    const increaseClass = increase === true ? 'increase' : '';
+    const riseClass = rise === true ? 'like' : '';
+
+    return (
+      <li
+        className={`list-group-item d-flex justify-content-between ${increaseClass} ${riseClass}`}>
+        <span className="list-group-item-label" onClick={this.onClick}>{name}</span>
+        <input type="text" className="list-group-item-input" defaultValue={`${salary}$`} />
+        <div className="d-flex justify-content-center align-items-center">
+          <button type="button"
+            className="btn-cookie btn-sm "
+            onClick={this.onIncrease}>
+            <i className="fas fa-cookie"></i>
+          </button>
+
+          <button type="button" className="btn-trash btn-sm ">
+            <i className="fas fa-trash"></i>
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
 };
 
 export default EmployeesListItem;
